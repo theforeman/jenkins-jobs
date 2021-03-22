theforeman/jenkins-jobs

commit f0d939ada7b9d4e380ba058d17e65376a1408a99

push katello RPMs to fedorapeople AND yum.tfm

theforeman.org/pipelines/lib/release.groovy
@@ -29,6 +29,10 @@ void push_pulpcore_rpms(version, distro) {
     push_rpms("pulpcore-${version}", "pulpcore", version, distro, true)
 }
 
+void push_katello_rpms(version, distro) {
+    push_rpms("katello-${version}", "katello", version, distro, true)
+}
+
 void mash(collection, version) {
     sshagent(['mash']) {
         sh "ssh -o 'BatchMode yes' root@koji.katello.org collection-mash-split.py ${collection} ${version}"

theforeman.org/pipelines/release/pipelines/katello.groovy
@@ -44,6 +44,11 @@ pipeline {
 
             steps {
                 push_rpms_katello(katello_version)
+                script {
+                    foreman_el_releases.each { distro ->
+                        push_katello_rpms(katello_version, distro)
+                    }
+                }
             }
         }
     }

commit f033e3f57c8521fcc791d8089dd7ea3e69c08859

correct katello paths
katello repos have sub-components

Fixes: f0d939ada7b9d4e380ba058d17e65376a1408a99

theforeman.org/pipelines/lib/release.groovy
@@ -30,7 +30,8 @@ void push_pulpcore_rpms(version, distro) {
 }
 
 void push_katello_rpms(version, distro) {
-    push_rpms("katello-${version}", "katello", version, distro, true)
+    push_rpms("katello-${version}/katello", "katello/katello", version, distro, true)
+    push_rpms("katello-${version}/candlepin", "katello/candlepin", version, distro, true)
 }
 
 void mash(collection, version) {