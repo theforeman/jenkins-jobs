theforeman/jenkins-jobs

commit da2903bcd68725b68778d3dbaa4e1a66cd32869a

Refactor out setup sources script for debian and add single execute script
This change pulls the common execution of pbduiler into a stand-alone
script and creates focused setup sources scripts per use case.

theforeman.org/scripts/debian/setup_sources_plugin.sh
@@ -1,0 +1,65 @@
+#!/bin/bash
+set -xe
+
+# This script clones the git repos and cleans them up
+echo "--Setting Up Sources"
+
+# Setup the debian files
+cd plugins
+mkdir build-${project}
+cd build-${project}
+
+BUILD_TYPE=gem
+# Import variables from the project, allowing it to override behaviour
+if [ -e ../${project}/build_vars.sh ]; then
+  . ../${project}/build_vars.sh
+fi
+
+if [[ $project == *"smart_proxy"* ]]; then
+  # Figure out package version and gem2deb it
+  VERSION=$(head -n1 ../${project}/changelog|awk '{print $2}'|sed 's/(//;s/)//'|cut -f1 -d-)
+  gem fetch ${project} -v "=${VERSION}"
+  ../../dependencies/gem2deb ${project}-${VERSION}.gem --debian-subdir ../${project} --only-source-dir
+
+  # Should only be one dir generated by gem2deb
+  DIR=$(find -maxdepth 1 -type d -not -name '.')
+  echo $DIR
+  cd $DIR
+else
+  VERSION=$(head -n1 ../${project}/debian/changelog|awk '{print $2}'|sed 's/(//;s/)//'|cut -f1 -d-|cut -d: -f2)
+  if [[ $BUILD_TYPE == "gem" ]]; then
+    cp -r ../${project} ./
+    cd ${project}
+    ../../download_gems
+  elif [[ $BUILD_TYPE == "ansible-collection" ]]; then
+    COLLECTION=${project#ansible-collection-}
+    wget "https://galaxy.ansible.com/download/${COLLECTION}-${VERSION}.tar.gz"
+    mv "${COLLECTION}-${VERSION}.tar.gz" "${project}_${VERSION}.orig.tar.gz"
+    cp -r ../${project} ./
+    tar -x -C "${project}" -f "${project}_${VERSION}.orig.tar.gz"
+    cd ${project}
+  else
+    echo "Unsupported build type: ${BUILD_TYPE}"
+    exit 1
+  fi
+fi
+
+# Add changelog entry if this is a git/nightly build
+if [[ $gitrelease == true ]] || [[ -z $pr_number ]]; then
+  PACKAGE_NAME=$(head -n1 debian/changelog|awk '{print $1}')
+  LAST_COMMIT=$(git rev-list HEAD|/usr/bin/head -n 1)
+  DATE=$(date -R)
+  RELEASE="9999-plugin+scratchbuild+${BUILD_TIMESTAMP}"
+  MAINTAINER="${repoowner} <no-reply@theforeman.org>"
+  mv debian/changelog debian/changelog.tmp
+  echo "$PACKAGE_NAME ($RELEASE) UNRELEASED; urgency=low
+
+  * Automatically built package based on the state of
+    foreman-packaging at commit $LAST_COMMIT
+
+ -- $MAINTAINER  $DATE
+" > debian/changelog
+
+  cat debian/changelog.tmp >> debian/changelog
+  rm -f debian/changelog.tmp
+fi

theforeman.org/scripts/debian/execute_pbuilder.sh
@@ -1,98 +1,12 @@
 #!/bin/bash
 set -xe
 
-# This script clones the git repos, cleans them up, and builds the package
-echo "--Setting Up Sources"
-
-# Setup the debian files, figure out the version
-echo `git log -n1 --oneline`
-cd debian/${os}/
-VERSION=$(head -n1 ${project}/changelog|awk '{print $2}'|sed 's/(//;s/)//'|cut -f1 -d-)
-
-# Setup sources
-mkdir build-${project} && cd build-${project}
-if [[ x$repo =~ ^xdevelop ]]; then
-  url_base='https://ci.theforeman.org'
-  job_name=$nightly_jenkins_job  # e.g. 'test_develop', from triggering job
-  job_id=$nightly_jenkins_job_id # e.g. '123', from triggering
-  json_url="${url_base}/job/${job_name}/${job_id}/api/json"
-
-  # If a last* alias was used, resolve the numeric job ID
-  job_id=$(curl "${json_url}" | /usr/local/bin/JSON.sh -b | awk '$1 == "[\"number\"]" { print $2 }')
-  base_url=`curl "${json_url}" | /usr/local/bin/JSON.sh -b | awk '$1 ~ /^\["runs",.*,"number"\]/ && $2 == '$job_id' {getline; print $2; exit}' | tr -d \"`
-  if [ x$base_url = x ] ; then
-    base_url=`curl "${json_url}" | /usr/local/bin/JSON.sh -b | egrep '\["url"\]' | awk '{print $NF}' | tr -d \"`
-  fi
-  url="${base_url}/artifact/*zip*/archive.zip"
-
-  wget $url
-  unzip archive.zip
-  mv archive/pkg/*bz2 ${project}_${VERSION}.orig.tar.bz2
-
-  # Set this in case we need it
-  LAST_COMMIT=`curl "${json_url}" | /usr/local/bin/JSON.sh -b | egrep '"lastBuiltRevision","SHA1"' | awk '{print $NF}' | tr -d \" | head -n1`
-else
-  VERSION=`echo ${VERSION} | tr '~rc' '-rc'`
-  # Download sources
-  wget http://downloads.theforeman.org/${project}/${project}-${VERSION}.tar.bz2
-  wget http://downloads.theforeman.org/${project}/${project}-${VERSION}.tar.bz2.sig
-
-  # Verify with packaging key - commented until we can handle multiple keys
-#  tmp_keyring="./1AA043B8-keyring.gpg"
-#  gpg --no-default-keyring --keyserver keys.gnupg.net --keyring $tmp_keyring --recv-keys 1AA043B8
-#  if gpg --no-default-keyring --keyring $tmp_keyring ${project}-${VERSION}.tar.bz2.sig 2>&1 | grep -q "gpg: Good signature from \"Foreman Automatic Signing Key (2014) <packages@theforeman.org>\"" ; then
-#    true # ok
-#  else
-#    exit 2
-#  fi
-  mv ${project}-${VERSION}.tar.bz2 ${project}_${VERSION}.orig.tar.bz2
-
-  # Set this for test builds
-  LAST_COMMIT=${VERSION}
-fi
-
-# Unpack
-tar xvjf ${project}_${VERSION}.orig.tar.bz2
-if [[ -d ${project}-${VERSION}-develop ]] ; then
-	mv ${project}-${VERSION}-develop ${project}-${VERSION}
-fi
-
-# Bring in the debian packaging files
-cp -r ../${project} ./${project}-${VERSION}/debian
-cd ${project}-${VERSION}
-
-# Add changelog entry if this is a git/nightly build
-if [ x$gitrelease = xtrue ] || [ x$pr_number != x ]; then
-  PACKAGE_NAME=$(head -n1 debian/changelog|awk '{print $1}')
-  DATE=$(date -R)
-  RELEASE="9999-${os}+scratchbuild+${BUILD_TIMESTAMP}"
-  MAINTAINER="${repoowner} <no-reply@theforeman.org>"
-  mv debian/changelog debian/changelog.tmp
-  echo "$PACKAGE_NAME ($RELEASE) UNRELEASED; urgency=low
-
-  * Automatically built package based on the state of
-    foreman-packaging at commit $LAST_COMMIT
-
- -- $MAINTAINER  $DATE
-" > debian/changelog
-
-  cat debian/changelog.tmp >> debian/changelog
-  rm -f debian/changelog.tmp
-
-  # rename orig tarball to stop lintian complaining
-  mv ../${project}_${VERSION}.orig.tar.bz2 ../${project}_9999.orig.tar.bz2
-fi
+# This script builds the package
+echo "--Execute pdebuild"
 
 # Build the package for the OS using pbuilder
 # needs sudo as pedebuild uses loop and bind mounts
-if [ $arch = x86 ]; then
-  sudo pdebuild-${os}64
-else
-  # Only build on non-x86 arches when the binary differs
-  if grep -qe "Architecture:\s\+any" debian/control; then
-    sudo pdebuild-${os}
-  fi
-fi
+sudo pdebuild-${os}64
 
 # Cleanup, pdebuild uses root
 sudo chown -R jenkins:jenkins $WORKSPACE

theforeman.org/scripts/debian/execute_pbuilder_plugin.sh
@@ -1,58 +1,0 @@
-#!/bin/bash
-set -xe
-
-# This script clones the git repos, cleans them up, and builds the package
-echo "--Setting Up Sources"
-
-# Setup the debian files
-cd plugins
-mkdir build-${project}
-cd build-${project}
-
-BUILD_TYPE=gem
-# Import variables from the project, allowing it to override behaviour
-if [ -e ../${project}/build_vars.sh ]; then
-  . ../${project}/build_vars.sh
-fi
-
-VERSION=$(head -n1 ../${project}/debian/changelog|awk '{print $2}'|sed 's/(//;s/)//'|cut -f1 -d-|cut -d: -f2)
-if [[ $BUILD_TYPE == "gem" ]]; then
-  cp -r ../${project} ./
-  cd ${project}
-  ../../download_gems
-elif [[ $BUILD_TYPE == "ansible-collection" ]]; then
-  COLLECTION=${project#ansible-collection-}
-  wget "https://galaxy.ansible.com/download/${COLLECTION}-${VERSION}.tar.gz"
-  mv "${COLLECTION}-${VERSION}.tar.gz" "${project}_${VERSION}.orig.tar.gz"
-  cp -r ../${project} ./
-  tar -x -C "${project}" -f "${project}_${VERSION}.orig.tar.gz"
-  cd ${project}
-else
-  echo "Unsupported build type: ${BUILD_TYPE}"
-  exit 1
-fi
-
-# Add changelog entry if this is a git/nightly build
-if [ x$gitrelease = xtrue ] || [ x$pr_number != x ]; then
-  PACKAGE_NAME=$(head -n1 debian/changelog|awk '{print $1}')
-  DATE=$(date -R)
-  RELEASE="9999-plugin+scratchbuild+${BUILD_TIMESTAMP}"
-  MAINTAINER="${repoowner} <no-reply@theforeman.org>"
-  mv debian/changelog debian/changelog.tmp
-  echo "$PACKAGE_NAME ($RELEASE) UNRELEASED; urgency=low
-
-  * Automatically built package based on the state of
-    foreman-packaging at commit $LAST_COMMIT
-
- -- $MAINTAINER  $DATE
-" > debian/changelog
-
-  cat debian/changelog.tmp >> debian/changelog
-  rm -f debian/changelog.tmp
-fi
-
-# Build plugin
-sudo pdebuild-${os}64
-
-# Cleanup, pdebuild uses root
-sudo chown -R jenkins:jenkins $WORKSPACE

theforeman.org/scripts/debian/execute_pbuilder_proxy_plugin.sh
@@ -1,50 +1,0 @@
-#!/bin/bash
-set -xe
-
-# Create the package - assumes new gem2deb is in the packaging repo
-git checkout origin/deb/${repo} -b local
-cd plugins/
-mkdir build-${project}
-cd build-${project}
-
-# Import variables from the project, allowing it to override gem2deb behaviour
-if [ -e ../${project}/build_vars.sh ]; then
-  . ../${project}/build_vars.sh
-fi
-
-# Figure out package version and gem2deb it
-VERSION=$(head -n1 ../${project}/changelog|awk '{print $2}'|sed 's/(//;s/)//'|cut -f1 -d-)
-gem fetch ${project} -v "=${VERSION}"
-../../dependencies/gem2deb ${project}-${VERSION}.gem --debian-subdir ../${project} --only-source-dir
-
-# Should only be one dir generated by gem2deb
-DIR=$(find -maxdepth 1 -type d -not -name '.')
-echo $DIR
-cd $DIR
-
-# Add changelog entry if this is a git build
-if [ x$gitrelease = xtrue ]; then
-  PACKAGE_NAME=$(head -n1 debian/changelog|awk '{print $1}')
-  LAST_COMMIT=$(git rev-list HEAD|/usr/bin/head -n 1)
-  DATE=$(date -R)
-  RELEASE="${VERSION}-plugin+scratchbuild${BUILD_TIMESTAMP}"
-  MAINTAINER="${repoowner} <no-reply@theforeman.org>"
-  mv debian/changelog debian/changelog.tmp
-  echo "$PACKAGE_NAME ($RELEASE) UNRELEASED; urgency=low
-
-  * Automatically built package based on the state of
-    foreman-packaging at commit $LAST_COMMIT
-
- -- $MAINTAINER  $DATE
-" > debian/changelog
-
-  cat debian/changelog.tmp >> debian/changelog
-  rm -f debian/changelog.tmp
-fi
-
-# Build the package for the OS using pbuilder
-# needs sudo as pedebuild uses loop and bind mounts
-sudo pdebuild-${os}64
-
-# Cleanup, pdebuild uses root
-sudo chown -R jenkins:jenkins $WORKSPACE

theforeman.org/scripts/debian/setup_sources_core.sh
@@ -1,0 +1,83 @@
+#!/bin/bash
+set -xe
+
+# This script clones the git repos and cleans them up
+echo "--Setting Up Sources"
+
+# Setup the debian files, figure out the version
+echo `git log -n1 --oneline`
+cd debian/${os}/
+VERSION=$(head -n1 ${project}/changelog|awk '{print $2}'|sed 's/(//;s/)//'|cut -f1 -d-)
+
+# Setup sources
+mkdir build-${project} && cd build-${project}
+if [[ x$repo =~ ^xdevelop ]]; then
+  url_base='https://ci.theforeman.org'
+  job_name=$nightly_jenkins_job  # e.g. 'test_develop', from triggering job
+  job_id=$nightly_jenkins_job_id # e.g. '123', from triggering
+  json_url="${url_base}/job/${job_name}/${job_id}/api/json"
+
+  # If a last* alias was used, resolve the numeric job ID
+  job_id=$(curl "${json_url}" | /usr/local/bin/JSON.sh -b | awk '$1 == "[\"number\"]" { print $2 }')
+  base_url=`curl "${json_url}" | /usr/local/bin/JSON.sh -b | awk '$1 ~ /^\["runs",.*,"number"\]/ && $2 == '$job_id' {getline; print $2; exit}' | tr -d \"`
+  if [ x$base_url = x ] ; then
+    base_url=`curl "${json_url}" | /usr/local/bin/JSON.sh -b | egrep '\["url"\]' | awk '{print $NF}' | tr -d \"`
+  fi
+  url="${base_url}/artifact/*zip*/archive.zip"
+
+  wget $url
+  unzip archive.zip
+  mv archive/pkg/*bz2 ${project}_${VERSION}.orig.tar.bz2
+
+  # Set this in case we need it
+  LAST_COMMIT=`curl "${json_url}" | /usr/local/bin/JSON.sh -b | egrep '"lastBuiltRevision","SHA1"' | awk '{print $NF}' | tr -d \" | head -n1`
+else
+  VERSION=`echo ${VERSION} | tr '~rc' '-rc'`
+  # Download sources
+  wget https://downloads.theforeman.org/${project}/${project}-${VERSION}.tar.bz2 https://downloads.theforeman.org/${project}/${project}-${VERSION}.tar.bz2.sig
+
+  # Verify with packaging key - commented until we can handle multiple keys
+#  tmp_keyring="./1AA043B8-keyring.gpg"
+#  gpg --no-default-keyring --keyserver keys.gnupg.net --keyring $tmp_keyring --recv-keys 1AA043B8
+#  if gpg --no-default-keyring --keyring $tmp_keyring ${project}-${VERSION}.tar.bz2.sig 2>&1 | grep -q "gpg: Good signature from \"Foreman Automatic Signing Key (2014) <packages@theforeman.org>\"" ; then
+#    true # ok
+#  else
+#    exit 2
+#  fi
+  mv ${project}-${VERSION}.tar.bz2 ${project}_${VERSION}.orig.tar.bz2
+
+  # Set this for test builds
+  LAST_COMMIT=${VERSION}
+fi
+
+# Unpack
+tar xvjf ${project}_${VERSION}.orig.tar.bz2
+if [[ -d ${project}-${VERSION}-develop ]] ; then
+	mv ${project}-${VERSION}-develop ${project}-${VERSION}
+fi
+
+# Bring in the debian packaging files
+cp -r ../${project} ./${project}-${VERSION}/debian
+cd ${project}-${VERSION}
+
+# Add changelog entry if this is a git/nightly build
+if [[ $gitrelease == true ]] || [[ -n $pr_number ]]; then
+  PACKAGE_NAME=$(head -n1 debian/changelog|awk '{print $1}')
+  DATE=$(date -R)
+  RELEASE="9999-${os}+scratchbuild+${BUILD_TIMESTAMP}"
+  MAINTAINER="${repoowner} <no-reply@theforeman.org>"
+  mv debian/changelog debian/changelog.tmp
+  echo "$PACKAGE_NAME ($RELEASE) UNRELEASED; urgency=low
+
+  * Automatically built package based on the state of
+    foreman-packaging at commit $LAST_COMMIT
+
+ -- $MAINTAINER  $DATE
+" > debian/changelog
+
+  cat debian/changelog.tmp >> debian/changelog
+  rm -f debian/changelog.tmp
+
+  # rename orig tarball to stop lintian complaining
+  mv ../${project}_${VERSION}.orig.tar.bz2 ../${project}_9999.orig.tar.bz2
+fi

theforeman.org/scripts/debian/setup_sources_dependency.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -xe
 
+echo "--Setting Up Sources"
+
 # Create the package - assumes new gem2deb is in the packaging repo
-git checkout origin/deb/${repo} -b local
 cd dependencies/${os}
 mkdir build-${project}
 cd build-${project}
@@ -53,17 +54,3 @@ if [ x$gitrelease = xtrue ]; then
   cat debian/changelog.tmp >> debian/changelog
   rm -f debian/changelog.tmp
 fi
-
-# Build the package for the OS using pbuilder
-# needs sudo as pedebuild uses loop and bind mounts
-if [ $arch = x86 ]; then
-  sudo pdebuild-${os}64
-else
-  # Only build on non-x86 arches when the binary differs
-  if grep -qe "Architecture:\s\+any" debian/control; then
-    sudo pdebuild-${os}
-  fi
-fi
-
-# Cleanup, pdebuild uses root
-sudo chown -R jenkins:jenkins $WORKSPACE

theforeman.org/yaml/builders/build_deb_coreproject.yaml
@@ -2,5 +2,6 @@
     name: build_deb_coreproject
     builders:
       - shell: !include-raw: scripts/debian/configure_pbuilder.sh
+      - shell: !include-raw: scripts/debian/setup_sources_core.sh
       - shell: !include-raw: scripts/debian/execute_pbuilder.sh
       - shell: !include-raw: scripts/debian/stage_packages.sh

theforeman.org/yaml/builders/build_deb_dependency.yaml
@@ -2,5 +2,6 @@
     name: build_deb_dependency
     builders:
       - shell: !include-raw: scripts/debian/configure_pbuilder.sh
-      - shell: !include-raw: scripts/debian/execute_pbuilder_dependency.sh
+      - shell: !include-raw: scripts/debian/setup_sources_dependency.sh
+      - shell: !include-raw: scripts/debian/execute_pbuilder.sh
       - shell: !include-raw: scripts/debian/stage_packages.sh

commit d22e67772517d9707ba2ad06a32e32ff8fcecad7

only add staging version if pr_number is non-empty
Fixes: da2903b

theforeman.org/scripts/debian/setup_sources_plugin.sh
@@ -45,7 +45,7 @@ else
 fi
 
 # Add changelog entry if this is a git/nightly build
-if [[ $gitrelease == true ]] || [[ -z $pr_number ]]; then
+if [[ $gitrelease == true ]] || [[ -n $pr_number ]]; then
   PACKAGE_NAME=$(head -n1 debian/changelog|awk '{print $1}')
   LAST_COMMIT=$(git rev-list HEAD|/usr/bin/head -n 1)
   DATE=$(date -R)