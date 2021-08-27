ddfc3cd921638d73ecbdb18ca6bc236b8c2d0042
Eric D. Helms <ericdhelms@gmail.com>
2021-08-27 11:55:19 -0400

    Make pbuilder add repos hook project specific

--- theforeman.org/scripts/debian/configure_pbuilder.sh
+++ theforeman.org/scripts/debian/configure_pbuilder.sh
@@ -5,23 +5,24 @@ set -xe
 echo "--Apt Hook Setup"
 
 pbuilder_hooksdir=/etc/pbuilder/${os}64/hooks
+addrepo_hook=${pbuilder_hooksdir}/F60-${project}-addforemanrepo
 
 # Cleanup any old hooks
-sudo rm -f ${pbuilder_hooksdir}/F60addforemanrepo
+sudo rm -f $addrepo_hook
 
 if [[ $repoowner != theforeman ]]; then
   # Add a stagingdeb repo to the sources too, if it exists
   if wget -O/dev/null -q http://stagingdeb.theforeman.org/dists/${os}/${repoowner}-${version} ; then
-    echo "echo deb http://stagingdeb.theforeman.org/ ${os} ${repoowner}-${version} >> /etc/apt/sources.list" | sudo tee -a ${pbuilder_hooksdir}/F60addforemanrepo > /dev/null
+    echo "echo deb http://stagingdeb.theforeman.org/ ${os} ${repoowner}-${version} >> /etc/apt/sources.list" | sudo tee -a $addrepo_hook > /dev/null
   fi
   if wget -O/dev/null -q http://stagingdeb.theforeman.org/dists/plugins/${repoowner} ; then
-    echo "echo deb http://stagingdeb.theforeman.org/ plugins ${repoowner} >> /etc/apt/sources.list" | sudo tee -a ${pbuilder_hooksdir}/F60addforemanrepo > /dev/null
+    echo "echo deb http://stagingdeb.theforeman.org/ plugins ${repoowner} >> /etc/apt/sources.list" | sudo tee -a $addrepo_hook > /dev/null
   fi
 fi
 
 # Use tee to get around sudo/redirection problems, with /dev/null to drop the stdout
-echo "echo deb http://deb.theforeman.org/ ${os} ${version} >> /etc/apt/sources.list" | sudo tee -a ${pbuilder_hooksdir}/F60addforemanrepo > /dev/null
-echo "echo deb http://deb.theforeman.org/ plugins ${version} >> /etc/apt/sources.list" | sudo tee -a ${pbuilder_hooksdir}/F60addforemanrepo > /dev/null
+echo "echo deb http://deb.theforeman.org/ ${os} ${version} >> /etc/apt/sources.list" | sudo tee -a $addrepo_hook > /dev/null
+echo "echo deb http://deb.theforeman.org/ plugins ${version} >> /etc/apt/sources.list" | sudo tee -a $addrepo_hook > /dev/null
 
 # Read Foreman package installation logs on failure
 echo "cat /var/log/foreman-install.log || true" | sudo tee ${pbuilder_hooksdir}/C10foremanlog
