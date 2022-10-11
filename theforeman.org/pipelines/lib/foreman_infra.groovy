theforeman/jenkins-jobs

commit e865f9d9f1f485e09a7eb007cf6b7f01315772fd

also switch "the big" foreman release pipe to duffy

theforeman.org/pipelines/lib/foreman_infra.groovy
@@ -139,6 +139,26 @@ def runDuffyPipeline(project, version, expected_version = '') {
     runCicoJob(job, parameters, extra_vars)
 }
 
+def runDuffyPipelines(projects, version, expected_version = '') {
+    def pipes = []
+
+    pipes += projects.collect { project ->
+        [
+            name: "${project}-${version}",
+            job: "foreman-pipeline-${project}-${version}",
+            parameters: [
+                expected_version: expected_version
+            ],
+            extra_vars: [
+                jenkins_download_artifacts: 'true',
+                jenkins_artifacts_directory: "${env.WORKSPACE}/artifacts/foreman-pipeline-${project}-${version}-${os}-${type}/",
+            ]
+        ]
+    }
+
+    runCicoJobsInParallel(pipes)
+}
+
 def notifyDiscourse(env, introText, description) {
     emailext(
         subject: "${env.JOB_NAME} ${env.BUILD_ID} failed",

theforeman.org/pipelines/release/pipelines/foreman-pipeline.groovy
@@ -35,7 +35,7 @@ pipeline {
 
             steps {
                 script {
-                    runCicoPipelines('foreman', foreman_version, pipelines, params.expected_version)
+                    runDuffyPipelines(['foreman-rpm', 'foreman-deb'], foreman_version, pipelines, params.expected_version)
                 }
             }
         }

commit 6f5aef1ab9d84ce0bd29216bbbdf61ad1392a95f

Only use known variables in artifact directory
Fixes: e865f9d9f1f485e09a7eb007cf6b7f01315772fd

theforeman.org/pipelines/lib/foreman_infra.groovy
@@ -151,7 +151,7 @@ def runDuffyPipelines(projects, version, expected_version = '') {
             ],
             extra_vars: [
                 jenkins_download_artifacts: 'true',
-                jenkins_artifacts_directory: "${env.WORKSPACE}/artifacts/foreman-pipeline-${project}-${version}-${os}-${type}/",
+                jenkins_artifacts_directory: "${env.WORKSPACE}/artifacts/foreman-pipeline-${project}-${version}/",
             ]
         ]
     }