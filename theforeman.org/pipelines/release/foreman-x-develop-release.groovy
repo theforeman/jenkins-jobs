theforeman/jenkins-jobs

commit ab3ef60e7d7ee32e5f7048621b71ac2de71cdfba

force build koji and build copr onto different agents

theforeman.org/pipelines/release/foreman-x-develop-release.groovy
@@ -8,7 +8,7 @@ def deb_build_steps = [
 ]
 
 pipeline {
-    agent { label 'rpmbuild' }
+    agent none
 
     options {
         timestamps()
@@ -21,6 +21,8 @@ pipeline {
         stage('Build Package') {
             parallel {
                 stage('Build Koji RPM') {
+                    agent { label 'rpmbuild' }
+
                     when {
                         expression { build_rpm }
                     }
@@ -60,6 +62,8 @@ pipeline {
                     }
                 }
                 stage('Build Copr RPM') {
+                    agent { label 'rpmbuild' }
+
                     when {
                         expression { build_rpm }
                     }

commit e024c033df707d90417424cdb75758aab342780e

move cleanup to the individual stages for koji/copr
the main stage doesn't have an agent anymore (and doesn't need one)

Fixes: ab3ef60e7d7ee32e5f7048621b71ac2de71cdfba

theforeman.org/pipelines/release/foreman-x-develop-release.groovy
@@ -60,6 +60,11 @@ pipeline {
                             }
                         }
                     }
+                    post {
+                        cleanup {
+                            deleteDir()
+                        }
+                    }
                 }
                 stage('Build Copr RPM') {
                     agent { label 'rpmbuild' }
@@ -103,6 +108,11 @@ pipeline {
                                     }
                                 }
                             }
+                        }
+                    }
+                    post {
+                        cleanup {
+                            deleteDir()
                         }
                     }
                 }
@@ -176,10 +186,6 @@ pipeline {
     post {
         failure {
             notifyDiscourse(env, "${project_name} package release pipeline failed:", currentBuild.description)
-        }
-        cleanup {
-            echo "Cleaning up workspace"
-            deleteDir()
         }
     }
 }