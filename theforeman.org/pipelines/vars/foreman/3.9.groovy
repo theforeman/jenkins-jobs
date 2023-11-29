theforeman/jenkins-jobs

commit 55c177cca3c740e7dae231b3fbcb7f51025f1e53

Add 3.9 and 4.11 jobs for branching

theforeman.org/pipelines/vars/foreman/3.9.groovy
@@ -1,0 +1,41 @@
+def foreman_version = '3.9'
+def git_branch = "${foreman_version}-stable"
+def stage_source = 'copr'
+
+def foreman_client_distros = [
+    'el9',
+    'el8',
+    'el7',
+    'sles12'
+]
+def foreman_el_releases = [
+    'el8'
+]
+def foreman_debian_releases = ['bullseye', 'focal']
+
+def pipelines_deb = [
+    'install': [
+        'debian11',
+        'ubuntu2004'
+    ],
+    'upgrade': [
+        'debian11',
+        'ubuntu2004'
+    ]
+]
+
+def pipelines_el = [
+    'install': [
+        'centos8-stream',
+        'almalinux8',
+    ],
+    'upgrade': [
+        'centos8-stream',
+        'almalinux8',
+    ]
+]
+
+def pipelines = [
+    'install': pipelines_deb['install'] + pipelines_el['install'],
+    'upgrade': pipelines_deb['upgrade'] + pipelines_el['upgrade'],
+]

theforeman.org/pipelines/vars/katello/4.11.groovy
@@ -1,0 +1,16 @@
+def foreman_version = '3.9'
+def katello_version = '4.11'
+def stage_source = 'copr'
+def foreman_el_releases = [
+    'el8'
+]
+def pipelines = [
+    'install': [
+        'centos8-stream',
+        'almalinux8',
+    ],
+    'upgrade': [
+        'centos8-stream',
+        'almalinux8',
+    ]
+]

theforeman.org/pipelines/test/foreman/vars.groovy
@@ -3,6 +3,10 @@ def foreman_integration_versions = [
         'ruby': ['2.7'],
         'katello': 'master'
     ],
+    '3.9-stable': [
+	'ruby': ['2.7'],
+	'katello': 'KATELLO-4.11'
+    ],
     '3.8-stable': [
         'ruby': ['2.7'],
         'katello': 'KATELLO-4.10'
@@ -40,6 +44,10 @@ def foreman_integration_versions = [
 def foreman_unit_versions = [
     'develop': [
         'ruby': ['2.7']
+    ],
+    '3.9-stable': [
+	'ruby': ['2.7'],
+	'katello': 'KATELLO-4.11'
     ],
     '3.8-stable': [
         'ruby': ['2.7'],

theforeman.org/pipelines/test/testKatello.groovy
@@ -3,6 +3,10 @@ def katello_versions = [
         'foreman': 'develop',
         'ruby': ['2.7']
     ],
+    'KATELLO-4.11': [
+	'foreman': '3.9-stable',
+	'ruby': ['2.7']
+    ],
     'KATELLO-4.10': [
         'foreman': '3.8-stable',
         'ruby': ['2.7']

centos.org/jobs/foreman-pipelines.yml
@@ -10,6 +10,7 @@
     version:
       - '3.7'
       - '3.8'
+      - '3.9'
       - nightly
     type:
       - foreman

theforeman.org/yaml/includes/foreman_versions.yaml.inc
@@ -1,2 +1,3 @@
 - '3.7'
 - '3.8'
+- '3.9'

theforeman.org/yaml/jobs/test_3_9_stable.yaml
@@ -1,0 +1,33 @@
+- job:
+    name: test_3_9_stable
+    project-type: matrix
+    quiet-period: 2700
+    properties:
+      - github_foreman
+    scm:
+      - foreman:
+          branch: '3.9-stable'
+    triggers:
+      - scm_fifteen_minutes
+      - github
+    axes:
+      - axis:
+          type: user-defined
+          name: ruby
+          values:
+            - 2.7
+      - axis:
+          type: label-expression
+          name: slave
+          values:
+            - fast
+    builders:
+      - test_develop
+    publishers:
+      - gemset_cleanup
+      - ircbot_freenode
+      - archive:
+          artifacts: 'pkg/*,Gemfile.lock,package-lock.json'
+          only-if-success: false
+      - junit:
+          results: 'jenkins/reports/unit/*.xml'

commit bb98fc86c455f127c975af0ee5bfa12aa0c09983

fix 3.9 jenkins job definitions
Fixes: 55c177cca3c740e7dae231b3fbcb7f51025f1e53

centos.org/jobs/katello-pipelines.yml
@@ -7,6 +7,7 @@
     version:
       - '4.9'
       - '4.10'
+      - '4.11'
       - 'nightly'
     type:
       - katello

theforeman.org/pipelines/vars/foreman/3.9.groovy
@@ -1,12 +1,11 @@
 def foreman_version = '3.9'
 def git_branch = "${foreman_version}-stable"
-def stage_source = 'copr'
+def stage_source = 'stagingyum'
 
 def foreman_client_distros = [
     'el9',
     'el8',
-    'el7',
-    'sles12'
+    'el7'
 ]
 def foreman_el_releases = [
     'el8'

theforeman.org/pipelines/vars/katello/4.11.groovy
@@ -1,6 +1,6 @@
 def foreman_version = '3.9'
 def katello_version = '4.11'
-def stage_source = 'copr'
+def stage_source = 'stagingyum'
 def foreman_el_releases = [
     'el8'
 ]

theforeman.org/yaml/includes/foreman_versions_copr.yaml.inc
@@ -1,1 +1,1 @@
-[]
+- '3.9'

theforeman.org/yaml/jobs/pipeline/katello-rpm-pipeline.yaml
@@ -20,3 +20,4 @@
     version:
       - '4.9'
       - '4.10'
+      - '4.11'

theforeman.org/yaml/views/release.yml
@@ -13,6 +13,7 @@
     minor:
         - '7'
         - '8'
+        - '9'
 
 - view:
     name: 'Foreman Nightly'

theforeman.org/pipelines/test/foreman/vars.groovy
@@ -4,8 +4,8 @@ def foreman_integration_versions = [
         'katello': 'master'
     ],
     '3.9-stable': [
-	'ruby': ['2.7'],
-	'katello': 'KATELLO-4.11'
+        'ruby': ['2.7'],
+        'katello': 'KATELLO-4.11'
     ],
     '3.8-stable': [
         'ruby': ['2.7'],
@@ -46,8 +46,8 @@ def foreman_unit_versions = [
         'ruby': ['2.7']
     ],
     '3.9-stable': [
-	'ruby': ['2.7'],
-	'katello': 'KATELLO-4.11'
+        'ruby': ['2.7'],
+        'katello': 'KATELLO-4.11'
     ],
     '3.8-stable': [
         'ruby': ['2.7'],

theforeman.org/pipelines/test/testKatello.groovy
@@ -4,8 +4,8 @@ def katello_versions = [
         'ruby': ['2.7']
     ],
     'KATELLO-4.11': [
-	'foreman': '3.9-stable',
-	'ruby': ['2.7']
+        'foreman': '3.9-stable',
+        'ruby': ['2.7']
     ],
     'KATELLO-4.10': [
         'foreman': '3.8-stable',