theforeman/jenkins-jobs

commit b63d262c84f7b42075ee7d3b2fb55b45d431afc7

add the contents of the plugins gemfile.d to the checkout

theforeman.org/pipelines/lib/foreman.groovy
@@ -5,6 +5,9 @@ def databaseFile(id) {
 
 def addGem() {
     writeFile(text: "gemspec :path => '../', :development_group => :dev", file: 'bundler.d/Gemfile.local.rb')
+    if(fileExists('../gemfile.d/')) {
+        sh "cat ../gemfile.d/*.rb >> bundler.d/Gemfile.local.rb"
+    }
 }
 
 def addSettings(settings) {

commit 77852d15d59e5a13b3091063880997eea38908cc

include a newline when writing gemfile.local.rb
Fixes: b63d262c84f7b42075ee7d3b2fb55b45d431afc7

theforeman.org/pipelines/lib/foreman.groovy
@@ -4,7 +4,7 @@ def databaseFile(id) {
 }
 
 def addGem() {
-    writeFile(text: "gemspec :path => '../', :development_group => :dev", file: 'bundler.d/Gemfile.local.rb')
+    writeFile(text: "gemspec :path => '../', :development_group => :dev\n", file: 'bundler.d/Gemfile.local.rb')
     if(fileExists('../gemfile.d/')) {
         sh "cat ../gemfile.d/*.rb >> bundler.d/Gemfile.local.rb"
     }