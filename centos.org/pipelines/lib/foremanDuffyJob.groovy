theforeman/jenkins-jobs

commit f35f1a3889a993c5e2f83e27773e04f1367dc4fe

add duffy-based luna pipe that runs everything on one node

centos.org/pipelines/lib/foremanDuffyJob.groovy
@@ -1,0 +1,140 @@
+pipeline {
+    agent { label 'cico-workspace' }
+
+    options {
+        timestamps()
+        ansiColor('xterm')
+        buildDiscarder(logRotator(numToKeepStr: '3'))
+    }
+
+    environment {
+        ANSIBLE_CALLBACK_WHITELIST = 'community.general.opentelemetry'
+    }
+
+    stages {
+        stage('Setup Environment') {
+            steps {
+                deleteDir()
+                git url: 'https://github.com/theforeman/forklift.git'
+
+                sh(label: 'pip install', script: 'pip3.8 install --user opentelemetry-api opentelemetry-sdk opentelemetry-exporter-otlp')
+                sh(label: 'duffy collection install', script: 'ansible-galaxy collection install --collections-path ~/.ansible/collections git+https://github.com/evgeni/evgeni.duffy')
+
+                sh label: 'configure duffy', script: '''
+                # we do not want to leak the key to the logs
+                set +x
+                mkdir -p ~/.config
+                echo -e "client:\\n  url: https://duffy.ci.centos.org/api/v1\\n  auth:\\n    name: foreman\\n    key: ${CICO_API_KEY}" > ~/.config/duffy
+                '''
+            }
+        }
+        stage('Provision Node') {
+            steps {
+                provisionDuffy()
+            }
+        }
+        stage('Install Pipeline Requirements') {
+            steps {
+                script {
+                    if (params.type == 'pulpcore') {
+                        setup_extra_vars = ['forklift_install_pulp_from_galaxy': true, 'forklift_install_from_galaxy': false, 'pipeline_version': params.version]
+                    } else {
+                        setup_extra_vars = ['forklift_telemetry': true]
+                    }
+                    runPlaybook(
+                        playbook: 'playbooks/setup_forklift.yml',
+                        inventory: duffy_inventory('./'),
+                        options: ['-b'],
+                        extraVars: setup_extra_vars,
+                        commandLineExtraVars: true,
+                    )
+                }
+            }
+        }
+        stage('Run Pipelines') {
+            steps {
+                script {
+                    def otel_env = """
+                    export OTEL_EXPORTER_OTLP_HEADERS='${env.OTEL_EXPORTER_OTLP_HEADERS}'
+                    export OTEL_EXPORTER_OTLP_ENDPOINT='${env.OTEL_EXPORTER_OTLP_ENDPOINT}'
+                    export OTEL_TRACES_EXPORTER='${env.OTEL_TRACES_EXPORTER}'
+                    export TRACE_ID='${env.TRACE_ID}'
+                    export SPAN_ID='${env.SPAN_ID}'
+                    export TRACEPARENT='${env.TRACEPARENT}'
+                    export TRACESTATE='${env.TRACESTATE}'
+                    export ANSIBLE_CALLBACK_WHITELIST='${env.ANSIBLE_CALLBACK_WHITELIST}'
+                    """
+
+                    writeFile(file: 'otel_env', text: otel_env)
+                    duffy_scp_in('otel_env', 'otel_env', 'duffy_box', './')
+                    sh(script: 'rm -rf otel_env', label: 'remove otel_env file')
+
+                    def branches = [:]
+                    pipelines.each { action, oses ->
+                        oses.each { os ->
+                            def name = "${os}-${action}"
+                            branches[name] = {
+                                def playBook = pipelineVars(action: action, type: params.type, version: params.version, os: os, extra_vars: ['expected_version': params.expected_version ?: ''])
+                                def extra_vars = buildExtraVars(extraVars: playBook['extraVars'] + ["forklift_vagrant_lock": "/tmp/forklift.lock"])
+                                def playbooks = duffy_ssh("ls forklift/pipelines/${playBook['pipeline']}", 'duffy_box', './', true)
+                                playbooks = playbooks.split("\n")
+
+                                for(playbook in playbooks) {
+                                    stage(playbook) {
+                                        duffy_ssh("source otel_env && cd forklift && ansible-playbook pipelines/${playBook['pipeline']}/${playbook} ${extra_vars}", 'duffy_box', './')
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    parallel branches
+
+                }
+            }
+        }
+    }
+
+    post {
+        always {
+            script {
+                def branches = [:]
+                pipelines.each { action, oses ->
+                    oses.each { os ->
+                        def name = "${os}-${action}-post"
+                        branches[name] = {
+                            def playBook = pipelineVars(action: action, type: params.type, version: params.version, os: os, extra_vars: ['expected_version': params.expected_version ?: ''])
+                            def extra_vars = buildExtraVars(extraVars: playBook['extraVars'])
+                            try {
+                                duffy_ssh("source otel_env && cd forklift && ansible-playbook playbooks/collect_debug.yml --limit '${playBook['boxes'].join(',')}' ${extra_vars}", 'duffy_box', './')
+                                runPlaybook(
+                                    playbook: 'jenkins-jobs/centos.org/ansible/fetch_debug_files.yml',
+                                    inventory: duffy_inventory('./'),
+                                    extraVars: ["workspace": "${env.WORKSPACE}/debug"] + playBook['extraVars'],
+                                    commandLineExtraVars: true,
+                                    options: ['-b']
+                                )
+                            } catch(Exception ex) {
+                                echo "Exception: ${ex}"
+                            }
+                        }
+                    }
+                }
+                parallel branches
+            }
+
+            archiveArtifacts artifacts: 'debug/**/*.tap', allowEmptyArchive: true
+            archiveArtifacts artifacts: 'debug/**/*.tar.xz', allowEmptyArchive: true
+            archiveArtifacts artifacts: 'debug/**/*.xml', allowEmptyArchive: true
+            archiveArtifacts artifacts: 'debug/**/report/**', allowEmptyArchive: true
+            archiveArtifacts artifacts: 'debug/**/foreman-backup/**', allowEmptyArchive: true
+
+            step([$class: "TapPublisher", testResults: "debug/**/*.tap"])
+            junit testResults: "debug/**/*.xml", allowEmptyResults: true
+
+        }
+        cleanup {
+            deprovisionDuffy()
+            deleteDir()
+        }
+    }
+}

centos.org/ansible/deprovision_duffy.yml
@@ -1,0 +1,8 @@
+---
+- hosts: localhost
+  vars:
+    duffy_session: "{{ lookup('file', './duffy_session') }}"
+  tasks:
+    - name: 'Destroy node'
+      evgeni.duffy.session_retire:
+        session_id: "{{ duffy_session }}"

centos.org/ansible/provision_duffy.yml
@@ -1,0 +1,38 @@
+---
+- hosts: localhost
+  tasks:
+    - name: 'Get a new node'
+      evgeni.duffy.session_request:
+        pool: metal-ec2-c5n-centos-8s-x86_64
+        quantity: 1
+      register: duffy_data
+
+    - name: 'Fail if no hosts returned'
+      fail:
+        msg: 'Could not get hosts from ci.centos.org'
+      when: duffy_data.nodes|length == 0
+
+    - name: 'Write data locally'
+      copy:
+        content: "{{ duffy_data.session }}"
+        dest: "{{ playbook_dir }}/duffy_session"
+
+    - name: 'Write inventory'
+      copy:
+        content: |
+          {% for host in duffy_data.nodes %}
+          {{ host['hostname'] }} ansible_fqdn={{ host['hostname'] }} ansible_user=root ansible_host={{ host['ipaddr'] }}
+          {% endfor %}
+        dest: "{{ playbook_dir }}/duffy_inventory"
+
+    - name: 'Fetch default ssh_config'
+      slurp:
+        src: /etc/ssh/ssh_config
+      register: etc_ssh_config
+
+    - name: 'Write ssh config file'
+      template:
+        src: templates/ssh_config_duffy.j2
+        dest: "{{ playbook_dir }}/ssh_config"
+      vars:
+        ssh_config_base: "{{ etc_ssh_config['content'] | b64decode }}"

centos.org/ansible/templates/ssh_config_duffy.j2
@@ -1,0 +1,11 @@
+{{ ssh_config_base }}
+{% for host in duffy_data.nodes %}
+
+Host duffy_box
+  HostName {{ host['hostname']  }}
+  User root
+  Port 22
+  StrictHostKeyChecking no
+  PasswordAuthentication no
+  GlobalKnownHostsFile=/dev/null
+{% endfor %}

centos.org/jobs/foreman-pipeline-template.yml
@@ -32,3 +32,32 @@
         - ../theforeman.org/pipelines/lib/duffy.groovy{empty}
         - ../theforeman.org/pipelines/lib/ansible.groovy{empty}
         - ../theforeman.org/pipelines/lib/foreman_infra.groovy{empty}
+
+- job-template:
+    name: foreman-pipeline-{type}-{version}
+    project-type: 'pipeline'
+    sandbox: true
+    parameters:
+      - string:
+          name: expected_version
+          default: ''
+      - string:
+          name: version
+          default: '{version}'
+      - string:
+          name: type
+          default: '{type}'
+    properties:
+      - build-discarder:
+          days-to-keep: 3
+          num-to-keep: 3
+          artifact-days-to-keep: 3
+          artifact-num-to-keep: 3
+    dsl:
+      !include-raw:
+        - ../theforeman.org/pipelines/vars/katello/{version}.groovy
+        - pipelines/lib/foremanDuffyJob.groovy{empty}
+        - pipelines/lib/pipelines.groovy{empty}
+        - ../theforeman.org/pipelines/lib/duffy.groovy{empty}
+        - ../theforeman.org/pipelines/lib/ansible.groovy{empty}
+        - ../theforeman.org/pipelines/lib/foreman_infra.groovy{empty}

centos.org/jobs/luna-pipelines.yml
@@ -14,3 +14,13 @@
       - upgrade
     type:
       - luna
+
+- project:
+    name: luna-nightly-duffy
+    jobs:
+      - 'foreman-pipeline-{type}-{version}'
+    empty: ''
+    version:
+      - nightly
+    type:
+      - luna

centos.org/pipelines/lib/pipelines.groovy
@@ -1,6 +1,10 @@
 // action, type, version, os, extra_vars = nil
 def pipelineVars(Map args) {
-    boxes = ["pipe-*"]
+    def box_prefix = "pipe-"
+    if (args.action == 'upgrade') {
+        box_prefix = "pipe-up-"
+    }
+    boxes = ["${box_prefix}${args.type}-*${args.version}-${args.os}"]
 
     extra_vars = [
         'pipeline_version': args.version,

theforeman.org/pipelines/lib/duffy.groovy
@@ -10,6 +10,17 @@ def provision() {
     }
 }
 
+def provisionDuffy() {
+    fix_ansible_config()
+    git_clone_jenkins_jobs(target_dir: 'jenkins-jobs')
+
+  	dir('jenkins-jobs/centos.org/ansible') {
+        runPlaybook(playbook: 'provision_duffy.yml')
+        archiveArtifacts artifacts: 'duffy_inventory'
+        archiveArtifacts artifacts: 'ssh_config'
+    }
+}
+
 def fix_ansible_config() {
     // the yaml stdout callback is gone in ansible-core
     sh(script: "sed -i /stdout_callback/d ansible.cfg", label: 'fix ansible config')
@@ -21,6 +32,18 @@ def deprovision() {
             runPlaybook(playbook: 'deprovision.yml')
       	}
     }
+}
+
+def deprovisionDuffy() {
+    if (fileExists('jenkins-jobs/centos.org/ansible/duffy_session')) {
+        dir('jenkins-jobs/centos.org/ansible') {
+            runPlaybook(playbook: 'deprovision_duffy.yml')
+      	}
+    }
+}
+
+def duffy_inventory(relative_dir = '') {
+    return relative_dir + 'jenkins-jobs/centos.org/ansible/duffy_inventory'
 }
 
 def cico_inventory(relative_dir = '') {

commit 8e047b3be048e127eafc3eea6fda8d2063ca2fb0

correctly pass foreman_expected_version to pipelines
Fixes: f35f1a3889a993c5e2f83e27773e04f1367dc4fe

centos.org/pipelines/lib/foremanDuffyJob.groovy
@@ -88,7 +88,7 @@ pipeline {
                             def username = "pipe-${os}-${action}"
                             def boxname = "${username}@duffy_box"
                             branches[name] = {
-                                def playBook = pipelineVars(action: action, type: params.type, version: params.version, os: os, extra_vars: ['expected_version': params.expected_version ?: ''])
+                                def playBook = pipelineVars(action: action, type: params.type, version: params.version, os: os, extra_vars: ['foreman_expected_version': params.expected_version ?: ''])
                                 def extra_vars = buildExtraVars(extraVars: playBook['extraVars'])
                                 def playbooks = duffy_ssh("ls forklift/pipelines/${playBook['pipeline']}", boxname, './', true)
                                 playbooks = playbooks.split("\n")
@@ -118,7 +118,7 @@ pipeline {
                         def username = "pipe-${os}-${action}"
                         def boxname = "${username}@duffy_box"
                         branches[name] = {
-                            def playBook = pipelineVars(action: action, type: params.type, version: params.version, os: os, extra_vars: ['expected_version': params.expected_version ?: ''])
+                            def playBook = pipelineVars(action: action, type: params.type, version: params.version, os: os, extra_vars: ['foreman_expected_version': params.expected_version ?: ''])
                             def extra_vars = buildExtraVars(extraVars: playBook['extraVars'])
                             try {
                                 duffy_ssh("source otel_env && cd forklift && ansible-playbook playbooks/collect_debug.yml --limit '${playBook['boxes'].join(',')}' ${extra_vars}", boxname, './')