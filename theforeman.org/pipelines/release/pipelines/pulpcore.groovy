theforeman/jenkins-jobs

commit cdfad4759b0f0e17cc8509b275fdcb34f8ff9bb1

Align pipelines on repoclosures() function
For client it still had Fedora compatiblity code, but that has been
dropped in Foreman 3.1 so no longer relevant.

theforeman.org/pipelines/release/pipelines/pulpcore.groovy
@@ -36,12 +36,7 @@ pipeline {
         stage('staging-repoclosure') {
             steps {
                 script {
-                    def parallelStagesMap = [:]
-                    def name = 'pulpcore-staging'
-                    pulpcore_distros.each { distro ->
-                        parallelStagesMap[distro] = { repoclosure(name, distro, pulpcore_version) }
-                    }
-                    parallel parallelStagesMap
+                    parallel repoclosures('pulpcore-staging', foreman_el_releases, foreman_version)
                 }
             }
             post {

theforeman.org/pipelines/release/pipelines/client.groovy
@@ -36,16 +36,7 @@ pipeline {
         stage('staging-repoclosure') {
             steps {
                 script {
-                    def parallelStagesMap = [:]
-                    def name = 'foreman-client-staging'
-                    foreman_client_distros.each { distro ->
-                        if (distro.startsWith('el')) {
-                            parallelStagesMap[distro] = { repoclosure(name, distro, foreman_version) }
-                        } else if (distro.startsWith('fc')) {
-                            parallelStagesMap[distro] = { repoclosure(name, distro.replace('fc', 'f'), foreman_version) }
-                        }
-                    }
-                    parallel parallelStagesMap
+                    parallel repoclosures('foreman-client-staging', foreman_client_distros, foreman_version)
                 }
             }
             post {

theforeman.org/pipelines/release/pipelines/katello.groovy
@@ -36,12 +36,7 @@ pipeline {
         stage('staging-repoclosure') {
             steps {
                 script {
-                    def parallelStagesMap = [:]
-                    def name = 'katello-staging'
-                    foreman_el_releases.each { distro ->
-                        parallelStagesMap[distro] = { repoclosure(name, distro, foreman_version) }
-                    }
-                    parallel parallelStagesMap
+                    parallel repoclosures('katello-staging', foreman_el_releases, foreman_version)
                 }
             }
             post {

theforeman.org/pipelines/release/pipelines/plugins.groovy
@@ -30,12 +30,7 @@ pipeline {
         stage('staging-repoclosure') {
             steps {
                 script {
-                    def parallelStagesMap = [:]
-                    def name = 'plugins-staging'
-                    foreman_el_releases.each { distro ->
-                        parallelStagesMap[distro] = { repoclosure(name, distro, foreman_version) }
-                    }
-                    parallel parallelStagesMap
+                    parallel repoclosures('plugins-staging', foreman_el_releases, foreman_version)
                 }
             }
             post {

commit a425e8acb9c65cfead3f7037da4676d7041b07ac

use correct vars for pulpcore repoclosure
Fixes: cdfad4759b0f0e17cc8509b275fdcb34f8ff9bb1

theforeman.org/pipelines/release/pipelines/pulpcore.groovy
@@ -36,7 +36,7 @@ pipeline {
         stage('staging-repoclosure') {
             steps {
                 script {
-                    parallel repoclosures('pulpcore-staging', foreman_el_releases, foreman_version)
+                    parallel repoclosures('pulpcore-staging', pulpcore_distros, pulpcore_version)
                 }
             }
             post {