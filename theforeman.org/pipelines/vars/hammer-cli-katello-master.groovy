6eefcd4023fea47ab9a4a2db596b16fe029f1192
Evgeni Golov <evgeni@golov.de>
2024-07-12 08:07:56 +0200

    use main instead of master for hammer-cli-katello

--- theforeman.org/pipelines/vars/hammer-cli-katello-master.groovy
+++ theforeman.org/pipelines/vars/hammer-cli-katello-master.groovy
@@ -1,5 +1,5 @@
 def git_url = 'https://github.com/Katello/hammer-cli-katello.git'
-def git_ref = 'master'
+def git_ref = 'main'
 def hammer_cli_git_repos = ['hammer-cli', 'hammer-cli-foreman', 'hammer-cli-foreman-tasks', 'hammer_cli_foreman_bootdisk']
 def rpm_source_package_name = 'rubygem-hammer_cli_katello'
 def project_name = 'hammer-cli-katello'
