3ff837a5cd759ea3e8cd2a4e6a1c24a1040e32df
Odilon Sousa <osousa@redhat.com>
2024-10-07 13:11:29 -0300

    Drop EL8 from Pulpcore RPM Packaging

--- theforeman.org/pipelines/vars/pulpcore/nightly.groovy
+++ theforeman.org/pipelines/vars/pulpcore/nightly.groovy
@@ -1,10 +1,9 @@
 def pulpcore_version = 'nightly'
-def pulpcore_distros = ['el8', 'el9']
+def pulpcore_distros = ['el9']
 def packaging_branch = 'rpm/develop'
 def pipelines = [
     'pulpcore': [
         'centos9-stream',
-        'almalinux8',
         'almalinux9',
     ]
 ]
