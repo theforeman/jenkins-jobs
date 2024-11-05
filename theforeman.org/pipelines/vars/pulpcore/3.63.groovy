e4f3e641c135b13bee520a6a914e8199fc2fb726
Odilon Sousa <osousa@redhat.com>
2024-11-05 18:41:37 -0300

    Add Pulpcore 3.63 Config

---
+++ theforeman.org/pipelines/vars/pulpcore/3.63.groovy
@@ -0,0 +1,9 @@
+def pulpcore_version = '3.63'
+def pulpcore_distros = ['el9']
+def packaging_branch = 'rpm/3.63'
+def pipelines = [
+    'pulpcore': [
+        'centos9-stream',
+        'almalinux9',
+    ]
+]
