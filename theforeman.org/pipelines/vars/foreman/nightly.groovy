03a793699a6187f93b15c9577716b5e99fc96e54
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2024-12-20 16:43:45 +0100

    Drop Debian 11 bullseye in nightly
    This replaces plugin building from bullseye to jammy because that's the
    now the oldest release we support.

--- theforeman.org/pipelines/vars/foreman/nightly.groovy
+++ theforeman.org/pipelines/vars/foreman/nightly.groovy
@@ -10,16 +10,14 @@ def foreman_client_distros = [
 def foreman_el_releases = [
     'el9'
 ]
-def foreman_debian_releases = ['bullseye', 'bookworm', 'jammy']
+def foreman_debian_releases = ['bookworm', 'jammy']
 
 def pipelines_deb = [
     'install': [
-        'debian11',
         'debian12',
         'ubuntu2204'
     ],
     'upgrade': [
-        'debian11',
         'debian12',
         'ubuntu2204'
     ]
