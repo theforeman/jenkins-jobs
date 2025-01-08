f790d4cf1a88bf513d262db5608497665368ef00
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2025-01-08 20:10:04 +0100

    Set trigger package release jobs instead of on source jobs
    This inverses the relationship, but it makes it easier because we
    already have the source name because that's where we copy the artifacts
    from.

--- theforeman.org/pipelines/release/source/hammer-cli-x.groovy
+++ theforeman.org/pipelines/release/source/hammer-cli-x.groovy
@@ -35,14 +35,6 @@ pipeline {
         }
     }
     post {
-        success {
-            build(
-                job: "${project_name}-${git_ref}-package-release",
-                propagate: false,
-                wait: false
-            )
-        }
-
         failure {
             notifyDiscourse(env, "${project_name} source release pipeline failed:", currentBuild.description)
         }
