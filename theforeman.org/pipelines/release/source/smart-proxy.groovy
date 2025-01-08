a4120fd6f7dc65d23b2c8331cf45cbfe2ba618ae
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2025-01-08 20:01:14 +0100

    Align the smart-proxy testing and source release jobs
    This attempts to align the stable testing job to the source release job
    as much as possible.

--- theforeman.org/pipelines/release/source/smart-proxy.groovy
+++ theforeman.org/pipelines/release/source/smart-proxy.groovy
@@ -1,9 +1,9 @@
 pipeline {
-    agent any
+    agent none
 
     options {
         timestamps()
-        timeout(time: 2, unit: 'HOURS')
+        timeout(time: 1, unit: 'HOURS')
         ansiColor('xterm')
         buildDiscarder(logRotator(daysToKeepStr: '7'))
     }
@@ -18,13 +18,22 @@ pipeline {
                         values '2.7.6', '3.0.4', '3.1.0'
                     }
                 }
+                environment {
+                    BUNDLE_WITHOUT = 'development'
+                }
                 stages {
                     stage("Clone repository") {
                         steps {
                             git url: git_url, branch: git_ref
                         }
                     }
-                    stage("Test Ruby") {
+                    stage('Install dependencies') {
+                        steps {
+                            bundleInstall(ruby)
+                            archiveArtifacts(artifacts: 'Gemfile.lock')
+                        }
+                    }
+                    stage('Run tests') {
                         environment {
                             // ci_reporters gem
                             CI_REPORTS = 'jenkins/reports/unit'
@@ -33,7 +42,12 @@ pipeline {
                             MINITEST_REPORTERS_REPORTS_DIR = 'jenkins/reports/unit'
                         }
                         steps {
-                            run_test(ruby: env.ruby)
+                            bundleExec(ruby, 'rake jenkins:unit')
+                        }
+                        post {
+                            always {
+                                junit testResults: 'jenkins/reports/unit/*.xml'
+                            }
                         }
                     }
                 }
@@ -45,6 +59,8 @@ pipeline {
             }
         }
         stage('Build and Archive Source') {
+            agent any
+
             steps {
                 dir(project_name) {
                     git url: git_url, branch: git_ref
@@ -58,6 +74,11 @@ pipeline {
                     sourcefile_paths = generate_sourcefiles(project_name: project_name, source_type: source_type)
                 }
             }
+            post {
+                always {
+                    deleteDir()
+                }
+            }
         }
     }
 
@@ -65,22 +86,5 @@ pipeline {
         failure {
             notifyDiscourse(env, "${project_name} source release pipeline failed:", currentBuild.description)
         }
-
-        cleanup {
-            deleteDir()
-        }
-    }
-}
-
-def run_test(args) {
-    def ruby = args.ruby
-
-    try {
-        sh "cp config/settings.yml.example config/settings.yml"
-        bundleInstall(ruby, "--without=development")
-        archiveArtifacts(artifacts: 'Gemfile.lock')
-        bundleExec(ruby, "rake jenkins:unit --trace")
-    } finally {
-        junit(testResults: 'jenkins/reports/unit/*.xml')
     }
 }
