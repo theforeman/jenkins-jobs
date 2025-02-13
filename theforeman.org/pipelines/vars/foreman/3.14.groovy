791b45cd2cd3baeeb235b3b9ab2bc6f31165475c
Patrick Creech <pcreech@redhat.com>
2025-02-13 10:56:29 -0500

    Foreman 3.14 branching

---
+++ theforeman.org/pipelines/vars/foreman/3.14.groovy
@@ -0,0 +1,39 @@
+def foreman_version = '3.14'
+def git_branch = "${foreman_version}-stable"
+
+def foreman_client_distros = [
+    'el10',
+    'el9',
+    'el8',
+]
+def foreman_el_releases = [
+    'el9'
+]
+def foreman_debian_releases = ['bookworm', 'jammy']
+
+def pipelines_deb = [
+    'install': [
+        'debian12',
+        'ubuntu2204'
+    ],
+    'upgrade': [
+        'debian12',
+        'ubuntu2204'
+    ]
+]
+
+def pipelines_el = [
+    'install': [
+        'centos9-stream',
+        'almalinux9',
+    ],
+    'upgrade': [
+        'centos9-stream',
+        'almalinux9',
+    ]
+]
+
+def pipelines = [
+    'install': pipelines_deb['install'] + pipelines_el['install'],
+    'upgrade': pipelines_deb['upgrade'] + pipelines_el['upgrade'],
+]
