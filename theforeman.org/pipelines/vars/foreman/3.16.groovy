3aa8224472be625a402aa7bbbcd0cf26e8b6f4b6
Ondřej Gajdušek <ogajduse@redhat.com>
2025-08-08 15:05:57 +0200

    Foreman 3.16 branching

---
+++ theforeman.org/pipelines/vars/foreman/3.16.groovy
@@ -0,0 +1,40 @@
+def foreman_version = '3.16'
+def git_branch = "${foreman_version}-stable"
+def ruby_version = '2.7.6'
+
+def foreman_client_distros = [
+    'el10',
+    'el9',
+    'el8',
+]
+def foreman_el_releases = [
+    'el9'
+]
+def foreman_debian_releases = ['bookworm', 'jammy']
+
+def pipelines_deb = [
+    'install': [
+        'debian12',
+        'ubuntu2204'
+    ],
+    'upgrade': [
+        'debian12',
+        'ubuntu2204'
+    ]
+]
+
+def pipelines_el = [
+    'install': [
+        'centos9-stream',
+        'almalinux9',
+    ],
+    'upgrade': [
+        'centos9-stream',
+        'almalinux9',
+    ]
+]
+
+def pipelines = [
+    'install': pipelines_deb['install'] + pipelines_el['install'],
+    'upgrade': pipelines_deb['upgrade'] + pipelines_el['upgrade'],
+]
